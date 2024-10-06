6e50816ac3e2d25b9b7c1bf98e72f2927d527055
Valeriia Kanavchenko <165295440+valerie-kan@users.noreply.github.com>
2024-10-06 18:36:17 +0300

    Add contact list and search box

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ContactForm from './components/contactForm/ContactForm'
+import SearchBox from './components/serchBox/SearchBox'
+import { ContactList } from './components/contactList/ContactList'
+import contactsData from '../contscts.json'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [contacts, setContacts] = useState(contactsData);
+  const [filteredInfo, setFilteredInfo] = useState('');
+
+  const filteredList = contacts.filter((contact) => contact.name.toLowerCase().includes(filteredInfo.toLowerCase().trim()));
 
   return (
-    <>
-
-    </>
+    <div className='mainContainer'>
+      <h1 className='mainTitle'>Phonebook</h1>
+      <ContactForm />
+      <SearchBox
+        filteredInfo={filteredInfo}
+        setFilteredInfo={setFilteredInfo} />
+      <ContactList
+        filteredList={filteredList} />
+    </div>
   )
 }
 
