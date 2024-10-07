bbb53e480f7eadf195056c114c5a9f53708801e5
Valeriia Kanavchenko <165295440+valerie-kan@users.noreply.github.com>
2024-10-07 17:53:14 +0300

    Add contact form and formik

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,55 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import contactsData from '../contscts.json'
+import { nanoid } from 'nanoid'
+
 import ContactForm from './components/contactForm/ContactForm'
 import SearchBox from './components/serchBox/SearchBox'
 import { ContactList } from './components/contactList/ContactList'
-import contactsData from '../contscts.json'
+
+
 
 function App() {
-  const [contacts, setContacts] = useState(contactsData);
+  const [contacts, setContacts] = useState(() => {
+    const contsFromLS = localStorage.getItem('contactsInfo');
+    const parsedConts = JSON.parse(contsFromLS) ?? contactsData;
+    
+    return parsedConts;
+  });
+  
   const [filteredInfo, setFilteredInfo] = useState('');
 
+  useEffect(() => {
+    const stringifiedConts = JSON.stringify(contacts);
+    localStorage.setItem('contactsInfo', stringifiedConts)
+  }, [contacts]);
+
   const filteredList = contacts.filter((contact) => contact.name.toLowerCase().includes(filteredInfo.toLowerCase().trim()));
+
+  const addNewContact = (newCont) => {
+    const newContInfo = {
+      ...newCont, 
+      id: nanoid(),
+    }
+    
+    setContacts((prevInfo) => [...prevInfo, newContInfo]);
+  }
+
+  const deleteContact = (delatedId) => {
+    const finalContList = contacts.filter((contact) => contact.id !== delatedId);
+    setContacts(finalContList);
+  }
 
   return (
     <div className='mainContainer'>
       <h1 className='mainTitle'>Phonebook</h1>
-      <ContactForm />
+      <ContactForm addNewContact={addNewContact} />
       <SearchBox
         filteredInfo={filteredInfo}
         setFilteredInfo={setFilteredInfo} />
       <ContactList
-        filteredList={filteredList} />
+        filteredList={filteredList}
+        deleteContact={deleteContact} />
     </div>
   )
 }
